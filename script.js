660047cf0ff21fd4045f90ba4a8fc00a603cfb85
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-01-15 16:02:47 -0700

    Replaced the randInt function with a randIntFromStr function

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const hyperbole = ['toledo', 'movie time', 'blue smurfs', 'frostbite', 'beagles'
 const defeated = ['foiled', 'robbed', 'bamboozled', 'fooled', 'captured', 'blown out of the sky'];
 const villains = ['the Joker', 'Penguin', 'Rupert Thorne', 'Bane', 'Catwoman', 'Ra\'s Al Ghul', 'Killer Kroc'];
 
-const randomIntFromArr = (arr) => {
-    randInt = Math.floor(Math.random() * arr.length);
-    return randInt;
+const randStrFromArr = (arr) => {
+    const randInt = Math.floor(Math.random() * arr.length);
+    const randStr = arr[randInt];
+
+    return randStr;
 }
 
 const holyMessageBatman = (hyperboleArr) => {
@@ -13,4 +15,4 @@ const holyMessageBatman = (hyperboleArr) => {
     console.log(`Holy ${hyperbole}, Batman!`);
 }
 
-holyMessageBatman(hyperbole);
+// holyMessageBatman(hyperbole);
