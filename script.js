4b7db705eaf232d347754091c76798e676f3ba49
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-01-15 15:59:41 -0700

    Added two more arrays, one for villains, and another for synonyms of the word defeated

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const hyperbole = ['toledo', 'movie time', 'blue smurfs', 'frostbite', 'beagles', 'space stations'];
+const defeated = ['foiled', 'robbed', 'bamboozled', 'fooled', 'captured', 'blown out of the sky'];
+const villains = ['the Joker', 'Penguin', 'Rupert Thorne', 'Bane', 'Catwoman', 'Ra\'s Al Ghul', 'Killer Kroc'];
 
 const randomIntFromArr = (arr) => {
     randInt = Math.floor(Math.random() * arr.length);
